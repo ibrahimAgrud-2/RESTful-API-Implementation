6880bd3f570ddc465917af1716495df1bc455e5f
ibrahim <ibrahimagrud7@gmail.com>
2026-09-17 09:25:44 +0300

    Post student completed1

--- API_Implementation/obj/Debug/net8.0/API_Implementation.AssemblyInfo.cs
+++ API_Implementation/obj/Debug/net8.0/API_Implementation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Implementation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+324ca0eb882c76696c753fae33cdc679116298dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+887d46d8ba51d181bdf32746cc7f5a8797c99ca5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Implementation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Implementation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
