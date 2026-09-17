887d46d8ba51d181bdf32746cc7f5a8797c99ca5
ibrahim <ibrahimagrud7@gmail.com>
2026-09-17 09:19:16 +0300

    Implementing library sample

--- API_Implementation/Controllers/StudentController.cs
+++ API_Implementation/Controllers/StudentController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Data;
 
 namespace API_Implementation.Controllers
@@ -60,23 +63,7 @@ namespace API_Implementation.Controllers
         }
 
 
-        //[HttpGet("{ID}",Name ="GetStudentById")]
-        //public ActionResult<Student> GetStudentByID(int ID)
-        //{
-        //    if(ID<1)
-        //    {
-        //        return BadRequest($"Not Accepted ID {ID}");
-        //    }
-
-        //    var student = StudentDataSimulation.StudentList.FirstOrDefault(student => student.ID == ID);
-        //    if (student==null)
-        //    {
-        //        return NotFound($"No Student with ID {ID}");
-        //    }
-        //    return Ok(student);
-        //}
         [HttpGet("{ID}", Name = "GetStudentById")]
-
         //Normalde default olarak sadeec 200 ok success kodu dokümante edilmiş olur. Ama bu fonksiyonsa 3 farklı durum var.
         //Yani 3 farklı dönüş tipi olaiblir. Bunu bu API dökümanstasyonuna eklemek için bu attribut'ları ekleriz
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -96,8 +83,40 @@ namespace API_Implementation.Controllers
             {
                 return NotFound($"No Student with ID {ID}");
             }
-            return Ok("no");
+            return Ok(student);
         }
 
+
+        //POST işlemlerinin endüstri standardı başarı kodu 200 OK değil, 201 Created'dır.
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public ActionResult<Student> AddNewStudent(Student newStudent)
+        {
+            if(newStudent==null||newStudent.FirstName==""||newStudent.LastName==""||newStudent.ID<0||newStudent.Grade<0||newStudent.Age<0)
+            {
+                return BadRequest("Data not valid");
+            }
+            StudentDataSimulation.StudentList.Add(newStudent);
+
+            /*Normalde bir obje ekledikten sonra ID'sini dönderirdik. 
+            API'da URL söz konusu oldığu için ID yerine direk URL'inin döndermek daha kullanışlı. Sonuçta o veriye erişmek için sadee ID'si yetmez tam bir EndPoint lazım. Mesela https://localhost:7140/api/Students/12 gibi dadece 12 döndermekten daha iyidir. Bu en Profesyonel yaklaşımdır yani API, client'a "bu kaynağa nasıl ulaşacağını" da söylemeli
+
+            createdAtRoute fonksiyonu 3 şeyi birlikte döndürebilir. 1-201 status code, 2- Oluşan veriye erişmek için tam URL'i Respons Body'de oluşan verini tam halini dönderir.
+            Asıl olay Oluşan URL'i nasıl döndürebileceğimiz. Şimdi bunu direk manual olarak ta vermenin yolları var ama profesyonel değil. Bunu yapmak için şöyle düşünüyoruz:
+            Bize ne lazım? Bize sadece Bir URL lazım. Oluşan kaynağın URL'i. Bunu nasıl alabiliriz? Bunu almanın en iyi yolu 
+            Enpoint URL'lerinin tutulduğu routing tablosuna gideriz. Bu tablo framework tarafında ilk program ilk başladığında endpointler taranarak oluşturulur. Bu tabloda şu şekilde URL'ler olur
+                    Route Adı           →  URL Kalıbı
+                    ─────────────────────────────────────
+                    "GetStudentById"     →  / api / Students /{ id}
+                    (isimsiz POST)       →  / api / Students
+            Burada görüleceği üzere bizim istediğimiz URL GetStudentById isimli attribute'ın URL'i. Bu yüzden GetStudentById'i ilk parametre olarka verdik. Framework'e URL oluşturmak için GetStudentById'in URL'ini al. GetStudentById'de ID parametresi olduğu için bunu da vermemiz gerek. İşte 2.parameter bu yüzden var. 2.Paramterde URL'de tam olarak hangi paramtere neyle eşleşecek söylüyoruz. Bu durumda tek bir paramtere olduğu için sadece ID = dedik. Şu da olabilidi new { classId = newStudent.ClassId, id = newStudent.ID } eğer attrubte'a  birden fazla paramtere olsadı. 3.Parametre newStudent objesi response body'de eklenecek.  Sonuç olacak CreatedAtRoute fonksiyonunun istediği tüm bilgileri vermiş olduk
+
+             */
+
+            return CreatedAtRoute("GetStudentById", new { ID=newStudent.ID},newStudent);
+
+        }
+        
     }
 }
--- API_Implementation/obj/Debug/net8.0/API_Implementation.AssemblyInfo.cs
+++ API_Implementation/obj/Debug/net8.0/API_Implementation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Implementation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d7f12a308768b7fbf089d7d44670ca1915a690")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+324ca0eb882c76696c753fae33cdc679116298dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Implementation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Implementation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
